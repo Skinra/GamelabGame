7a73fbafca53565c7f486c460b06ca224385f333
MarcusMini <marc.inthaamnouay@gmail.com>
2015-01-08 14:44:32 +0100

    Update common.js

--- Game/Script/Common/common.js
+++ Game/Script/Common/common.js
@@ -8,10 +8,10 @@ function Update () {
 
 	// Eric, tu dois update ce fichier lorsque tu auras finis de faire le système d'attaque.
 	// le but de ce script est de détruire l'objet buisson_pont lorsque tous les ennemies ont été détruit.	
-
+	var obj = GameObject.Find("buisson_pont");
 	var ennemy = GameObject.Find("Ennemy");
 	
 	if(!ennemy){
-		Destroy("buisson_pont");
+		Destroy(obj);
 	}
 }
