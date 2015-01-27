c811f5c74abfc7390acd7618190e7450c1a3b718
Heimanu <heimanu.strap@hotmail.fr>
2015-01-27 19:04:29 +0100

    Update shoot.js

--- Game/Script/Player/shoot.js
+++ Game/Script/Player/shoot.js
@@ -2,11 +2,14 @@
 var firerate : float = 0.5;
 var nextFire : float = 1.0;
 
+
 function Start () {
 
 }
 function Update () {
 	var player = gameObject.Find('Player');
+	var position = Vector2(player.transform.position.x + 0.9, player.transform.position.y);
+	var position2 = Vector2(player.transform.position.x + 0.5, player.transform.position.y);
 	/*var vec = player.transform.position.x + 1.0;
 	var vecy = player.transform.position.y + 1.0; */
 	
@@ -17,6 +20,9 @@ function Update () {
 		
 		var bullet : GameObject = Instantiate(Resources.Load('shoot_player'), player.transform.position ,player.transform.rotation);
 		//bullet.rigidbody2D.AddForce(1,1);
+		
+		// var position = Vector2(player.transform.position.x + 5.0, player.transform.position.y);
+		// var bullet : GameObject = Instantiate(Resources.Load('shoot_player'), position ,player.transform.rotation);
 		
 		var script = player.GetComponent(move);
 		var post = script.pos;
@@ -32,6 +38,7 @@ function Update () {
 		}
 		else if(post == 3){
 			bullet.rigidbody2D.AddForce(Vector3.up * 100);
+			
 		}
 		}
 	}
@@ -39,12 +46,30 @@ function Update () {
 	if(Input.GetKey(KeyCode.V)){
 		if(Time.time / 2 > nextFire){
 		
-		nextFire = Time.time / 2 + 2;
+		nextFire = Time.time / 2 + 1;
 		
-		var zone : GameObject = Instantiate(Resources.Load('Attaquezone'), player.transform.position ,player.transform.rotation);
+		var zone : GameObject = Instantiate(Resources.Load('attaquezone'), player.transform.position ,player.transform.rotation);
 		
+		}
+		}
 		
+	if(Input.GetKey(KeyCode.B)){
+		if(Time.time / 2 > nextFire){
 		
+		nextFire = Time.time / 2 + 1;
+		
+		var laser : GameObject = Instantiate(Resources.Load('Lazer5'), position ,player.transform.rotation);
+		
+		}
+		}
+		
+	if(Input.GetKey(KeyCode.C)){
+		if(Time.time / 2 > nextFire){
+		
+		nextFire = Time.time / 2 + 1;
+		
+		var explosion : GameObject = Instantiate(Resources.Load('explosion3'), position2 ,player.transform.rotation);
+				
 		}
 		}
 	
