6712a98934bb81de25a27484c9be385440ec97c1
MarcusMini <marc.inthaamnouay@gmail.com>
2015-02-02 11:30:55 +0100

    Update lightScript.js

--- Game/Script/Common/lightScript.js
+++ Game/Script/Common/lightScript.js
@@ -1,31 +1,43 @@
-var x : boolean;
+public var x : boolean ;
 var timer2 : float;
+var timer : float;
 var otherLight;
 var otherLight2;
 
 var minIntensity : float;
 var maxIntensity : float;
+var intensity : float;
 
 function Start(){
-	minIntensity = 0.0f;
-	maxIntensity = 1.8f;
-	x = true;
-	this.gameObject.light.intensity = minIntensity;
+	x = PlayerPrefsX.GetBool("xvalue");
+	timer2 = PlayerPrefs.GetInt("timer");
+		
+	if(x){
+		minIntensity = PlayerPrefs.GetInt("intensité");
+		maxIntensity = 1.8f;
+	}
+	else{
+		maxIntensity = PlayerPrefs.GetInt("intensité");
+		minIntensity = 0.0f;
+	}
 }
 
 function ambiantLight(){
+
 	timer2 += Mathf.Clamp(Time.deltaTime, 0.0, 30.0);
 	
-	if(timer2 > 30){
+	if(timer2 > 100){
 		x = !x;
 		timer2 = 0;
+		minIntensity = 0.0f;
+		maxIntensity = 1.8f;
 	}
 
 	if(x){
-		light.intensity = Mathf.Lerp(minIntensity, maxIntensity, timer2 * 0.1);
+		intensity = light.intensity = Mathf.Lerp(minIntensity, maxIntensity, timer2 * 0.1);
 	}
 	else if(!x){
-		light.intensity = Mathf.Lerp(maxIntensity, minIntensity, timer2 * 0.1);
+		intensity = light.intensity = Mathf.Lerp(maxIntensity, minIntensity, timer2 * 0.1);
 	}
 }
 
@@ -34,21 +46,25 @@ function Update () {
 	
 	otherLight = GameObject.Find('Point light 2');
 	otherLight2 = GameObject.Find('Point light');
+	timer += Time.deltaTime;
+	
 	
 	if(gameObject.light.intensity > 0.9){
 		if(otherLight){
-			otherLight.light.intensity = 0.0;
+			//otherLight.light.intensity = 0.0;
+			otherLight.light.intensity = Mathf.Lerp(3.0, 0.0, timer * 0.1);
 		}
 		if(otherLight2){
-			otherLight2.light.intensity = 0.0;
+			//otherLight2.light.intensity = 0.0;
+			otherLight2.light.intensity = Mathf.Lerp(3.0, 0.0, timer * 0.1);
 		}
 	}
 	else{
 		if(otherLight){
-			otherLight.light.intensity = 8.0;
+			otherLight.light.intensity = Mathf.Lerp(0.0, 3.0, timer * 0.1);
 		}
 		if(otherLight2){
-			otherLight2.light.intensity = 1.0;
+			otherLight2.light.intensity = Mathf.Lerp(0.0, 3.0, timer * 0.1);;
 		}
 	}	
 }
